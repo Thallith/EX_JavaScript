461a5cdaec51c732b046d358bb0a10839d7c59da
Thallith Souza <101523222+Thallith@users.noreply.github.com>
2022-12-02 02:17:36 -0300

    Add files via upload

---
+++ Exercicio01/Ex1.js
@@ -0,0 +1,23 @@
+function exibirpares(){
+    let v1=parseInt(document.getElementById('v1').value);
+    let res=document.getElementById('res')
+    res.innerHTML=""
+
+    for (let i=1; i<=v1; i++) {
+        if(i%2==0){
+            res.innerHTML+=i+"-"
+        } 
+    }
+}
+
+function exibirimpares(){
+    let v1=parseInt(document.getElementById('v1').value);
+    let res=document.getElementById('res')
+    res.innerHTML=""
+
+    for (let i=1; i<=v1; i++ ) {
+        if(i%2!==0) {
+            res.innerHTML+=i+"-"
+        }
+    }
+}
